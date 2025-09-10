c2a9161e2b05740f8a604aa09996e16246a0db9d
code wizard <codewizard@gmail.com>
2025-09-10 12:10:50 +0530

    Fix: updated package.json & vite.config.js for Render deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,14 @@
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),react()],
+    react(),
+    tailwindcss()
+  ],
+  build: {
+    outDir: 'dist'
+  }
 })
